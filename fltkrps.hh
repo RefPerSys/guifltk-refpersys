9681a111031b5893570162f17cbcf10a9ebdec3e
Basile Starynkevitch <basile@starynkevitch.net>
2023-04-06 21:24:10 +0200

    adding fltkrps.hh

---
+++ fltkrps.hh
@@ -0,0 +1,17 @@
+/****** file guifltk-refpersys/fltkrps.hh ******
+ * SPDX-License-Identifier: MIT
+ * © Copyright 2023 Basile Starynkevitch
+ ***************************************************/
+
+#ifndef  FLTKRPS_INCLUDED
+#define FLTKRPS_INCLUDED 1
+
+#ifndef GIT_ID
+#error GIT_ID should be defined by compilation command
+#endif
+
+#ifndef SHORTGIT_ID
+#error SHORTGIT_ID should be defined by compilation command
+#endif
+
+#endif /* FLTKRPS_INCLUDED */
