283ced13b8ba6c2dbb24693c9b4743b6329e6d5a
Basile Starynkevitch <basile@starynkevitch.net>
2023-04-15 08:51:00 +0200

    parse_program_options is incomplete and should be coded

--- mainfltk.cc
+++ mainfltk.cc
@@ -1,6 +1,6 @@
 /**** file guifltk-refpersys/mainfltk.cc ******
- ****  SPDX-License-Identifier: MIT ****** 
- * 
+ ****  SPDX-License-Identifier: MIT ******
+ *
  * © Copyright 2023 The  Reflective Persistent System Team
  * team@refpersys.org &   http://refpersys.org/
  *
@@ -10,7 +10,14 @@
 
 #include "fltkrps.hh"
 
+static void
+parse_program_options (int argc, char**argv)
+{
+#warning incomplete parse_program_options should use getopt
+} // end parse_program_options
+
 int main(int argc, char**argv)
 {
+    parse_program_options(argc, argv);
 } // end main
 
