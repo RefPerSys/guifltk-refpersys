70182b346ed043135646108b52d5ebf4644cfa0d
Basile Starynkevitch <basile@starynkevitch.net>
2023-04-15 08:52:31 +0200

    empty jsonrpsfltk.cc

--- jsonrpsfltk.cc
+++ jsonrpsfltk.cc
@@ -1,6 +1,6 @@
 /**** file guifltk-refpersys/jsonrpsfltk.cc ******
- ****  SPDX-License-Identifier: MIT ****** 
- * 
+ ****  SPDX-License-Identifier: MIT ******
+ *
  * © Copyright 2023 The  Reflective Persistent System Team
  * team@refpersys.org &   http://refpersys.org/
  *
@@ -9,3 +9,7 @@
  **********************************************/
 
 #include "fltkrps.hh"
+
+#warning guifltk-refpersys/jsonrpsfltk.cc is empty should contain JSONCPP related code
+
+/* TODO: add code to communicate by JSONRPC with refpersys */
